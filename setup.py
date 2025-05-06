03fbdb7e496dc2e420ec5a61cf170dc636f9dfbc
Swarup Ghosh <swarupg2@illinois.edu>
2025-05-06 13:38:06 +0530

    fix: Allow taming package to be imported upon pip install

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='taming-transformers',
     version='1.0.0+pt-2.0',
     description='Taming Transformers for High-Resolution Image Synthesis',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["taming*"]),
     install_requires=[
         'torch',
         'numpy',
