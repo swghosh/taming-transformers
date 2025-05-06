1bc124d2d91b2f1e1aabfcaf1c531c69d7b83ea0
Swarup Ghosh <swarupg2@illinois.edu>
2025-05-06 13:11:28 +0530

    Update to use 'v1.0.0+pt-2.0'
    helps identify/distinguish package being installed from other pip sources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taming-transformers',
-    version='0.0.1',
+    version='1.0.0+pt-2.0',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
     install_requires=[
